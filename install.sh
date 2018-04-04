c2e06bd0f119b8229dcb87d8a209f32ec4891287
Kang Hyojun <iam.kanghyojun@gmail.com>
2018-04-05 01:11:12 +0900

    Add git hooks

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+hookDirectory=".git/hooks";
+hooks="pre-commit pre-push"
+
+
+echo "Installing git hooks..."
+for hook in $hooks;
+do
+  if [ ! -f "$hookDirectory/$hook" ]; then
+    echo "$hook";
+    ln -s "$(readlink -f "./hooks/$hook")" "$(readlink -f "./$hookDirectory/$hook")";
+  else
+    echo "Skipping install \"$hook\"..."
+  fi
+done;
